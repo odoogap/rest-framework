97d1741eb7c1025dfb7df962be01fe2b62340566
Ajay Javiya <ajaviya@odoogap.com>
2025-07-17 10:10:24 +0530

    [REM] unwanted changes

--- graphql_base/controllers/main.py
+++ graphql_base/controllers/main.py
@@ -61,7 +61,7 @@ class GraphQLControllerMixin:
             return response
         except HttpQueryError as e:
             result = json_encode({"errors": [{"message": str(e)}]})
-            headers = dict(e.headers)
+            headers = dict(e.headers or {})
             headers["Content-Type"] = "application/json"
             response = http.request.make_response(result, headers=headers)
             response.status_code = e.status_code
